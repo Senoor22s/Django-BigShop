956ed9dd41a45139c3739200ead5907195d19076
Senoor22s <legendsdt2@gmail.com>
2025-08-10 20:22:55 +0330

    14

--- core/shop/templatetags/shop_tags.py
+++ core/shop/templatetags/shop_tags.py
@@ -3,18 +3,20 @@ from shop.models import ProductStatusType, ProductModel
 register = template.Library()
 
 
-@register.inclusion_tag("includes/latest-products.html")
-def show_latest_products():
+@register.inclusion_tag("includes/latest-products.html",takes_context=True)
+def show_latest_products(context):
+    request = context['request']
     latest_products = ProductModel.objects.filter(
         status=ProductStatusType.publish.value).order_by("-created_date")[:8]
-    return {"latest_products": latest_products}
+    return {"latest_products": latest_products,"request": request}
 
-@register.inclusion_tag("includes/similar-products.html")
-def show_similar_products(product):
+@register.inclusion_tag("includes/similar-products.html",takes_context=True)
+def show_similar_products(context, product):
+    request = context['request']
     product_categories = product.category.all()
     similar_prodcuts = ProductModel.objects.filter(
         status=ProductStatusType.publish.value,
         category__in=product_categories
     ).exclude(id=product.id).distinct().order_by("-created_date")[:4]
     
-    return {"similar_prodcuts": similar_prodcuts}
+    return {"similar_prodcuts": similar_prodcuts, "request": request}
