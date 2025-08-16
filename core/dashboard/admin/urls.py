ae5d9ecb69c650a2d568cef5c3ffdf8746787f2c
Senoor22s <legendsdt2@gmail.com>
2025-08-16 11:29:15 +0330

    25

--- core/dashboard/admin/urls.py
+++ core/dashboard/admin/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("secuirty-edit/",views.AdminSecurityEditView.as_view(),name="security-edit"),
     path("profile-edit/",views.AdminProfileEditView.as_view(),name="profile-edit"),
     path("profile/image/edit/",views.AdminProfileImageEditView.as_view(),name="profile-image-edit"),
+    path("product/list/",views.AdminProductListView.as_view(),name="product-list")
 ]
