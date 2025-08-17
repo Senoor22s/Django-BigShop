0c225483413c3cd1cb0901a55e6f538b7e8b9a48
Senoor22s <legendsdt2@gmail.com>
2025-08-17 15:44:10 +0330

    29

--- core/website/urls.py
+++ core/website/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+
 app_name = "website"
 
 urlpatterns = [
     path("",views.IndexView.as_view(),name="index"),
     path("contact/",views.ContactView.as_view(),name="contact"),
     path("about/",views.AboutView.as_view(),name="about"),
+    path('newsletter',views.NewsletterView.as_view(),name='newsletter'),
 ]
--- core/website/views.py
+++ core/website/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-# Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic.edit import FormView
+from .forms import NewsletterForm
+from django.contrib.messages.views import SuccessMessageMixin
 
 class IndexView(TemplateView):
     template_name = "website/index.html" 
@@ -10,3 +13,13 @@ class ContactView(TemplateView):
     
 class AboutView(TemplateView):
     template_name = "website/about.html" 
+
+class NewsletterView(SuccessMessageMixin,FormView):
+    template_name = 'index.html'
+    form_class = NewsletterForm
+    success_url = reverse_lazy('website:index')
+    success_message = "عضویت در خبرنامه با موفقیت انجام شد"
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
