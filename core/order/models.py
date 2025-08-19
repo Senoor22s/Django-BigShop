5cf40a0640c4164be43219cc8ab5ac19bead3c67
Senoor22s <legendsdt2@gmail.com>
2025-08-19 13:13:06 +0330

    35

--- core/order/models.py
+++ core/order/models.py
@@ -11,10 +11,12 @@ class OrderStatusType(models.IntegerChoices):
 
 class UserAddressModel(models.Model):
     user = models.ForeignKey('accounts.User',on_delete=models.CASCADE)
+    
     address = models.CharField(max_length=250)
     state = models.CharField(max_length=50)
     city = models.CharField(max_length=50)
     zip_code = models.CharField(max_length=50)
+
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
 
@@ -23,30 +25,46 @@ class CouponModel(models.Model):
     discount_percent = models.IntegerField(default=0,validators = [MinValueValidator(0),MaxValueValidator(100)])
     max_limit_usage = models.PositiveIntegerField(default=10)
     used_by = models.ManyToManyField('accounts.User',related_name = "coupon_users")
+    
     expiration_date = models.DateTimeField(null=True,blank=True)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
 
+
+
+# Create your models here.
 class OrderModel(models.Model):
     user = models.ForeignKey('accounts.User',on_delete=models.PROTECT)
+    
+    # order address information
     address = models.CharField(max_length=250)
     state = models.CharField(max_length=50)
     city = models.CharField(max_length=50)
     zip_code = models.CharField(max_length=50)
+    
+    
     total_price = models.DecimalField(default=0,max_digits=10,decimal_places=0)
     coupon = models.ForeignKey(CouponModel,on_delete=models.PROTECT,null=True,blank=True)
     status = models.IntegerField(choices=OrderStatusType.choices,default=OrderStatusType.pending.value)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
     
+    def calculate_total_price(self):
+        return sum(item.price * item.quantity for item in self.order_items.all())
+    
+    def __str__(self):
+        return f"{self.user.email} - {self.id}"
+    
+    
 class OrderItemModel(models.Model):
-    order = models.ForeignKey(OrderModel,on_delete=models.CASCADE) 
+    order = models.ForeignKey(OrderModel,on_delete=models.CASCADE,related_name="order_items") 
     product = models.ForeignKey('shop.ProductModel',on_delete=models.PROTECT)
     quantity = models.PositiveIntegerField(default=0)
     price = models.DecimalField(default=0,max_digits=10,decimal_places=0)
+    
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
     
     def __str__(self):
-        return f"{self.product.title} - {self.cart.id}"
+        return f"{self.product.title} - {self.order.id}"
     
--- core/order/views.py
+++ core/order/views.py
@@ -6,14 +6,51 @@ from django.views.generic import (
 from django.contrib.auth.mixins import LoginRequiredMixin
 from dashboard.permissions import HasCustomerAccessPermission
 from order.models import UserAddressModel
-from .forms import CheckOutForm
-from cart.models import CartModel
+from order.forms import CheckOutForm
+from cart.models import CartModel,CartItemModel
+from order.models import OrderModel,OrderItemModel
 from django.urls import reverse_lazy
+from cart.cart import CartSession
+
 
 class OrderCheckOutView(LoginRequiredMixin, HasCustomerAccessPermission,FormView):
     template_name = "order/checkout.html"
     form_class = CheckOutForm
     success_url = reverse_lazy('order:completed')
+    
+    def get_form_kwargs(self):
+        kwargs = super(OrderCheckOutView, self).get_form_kwargs()
+        kwargs['request'] = self.request
+        return kwargs
+    
+    def form_valid(self, form):
+        cleaned_data = form.cleaned_data
+        address = cleaned_data['address_id']
+        cart = CartModel.objects.get(user=self.request.user)
+        cart_items = cart.cart_items.all()
+        order = OrderModel.objects.create(
+            user=self.request.user,
+            address = address.address,
+            state = address.state,
+            city = address.city,
+            zip_code = address.zip_code,
+        )
+        for item in cart_items:
+            OrderItemModel.objects.create(
+                order = order,
+                product = item.product,
+                quantity = item.quantity,
+                price = item.product.get_price(),
+            )
+        cart_items.delete()
+        CartSession(self.request.session).clear()
+        order.total_price = order.calculate_total_price()
+        order.save()
+        return super().form_valid(form)
+    
+    def form_invalid(self, form):
+        print(self.request.POST)
+        return super().form_invalid(form)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -25,3 +62,6 @@ class OrderCheckOutView(LoginRequiredMixin, HasCustomerAccessPermission,FormView
         context["total_tax"] = total_tax
         context["total_last_price"] = total_price + total_tax
         return context
+
+class OrderCompletedView(LoginRequiredMixin, HasCustomerAccessPermission,TemplateView):
+    template_name = "order/completed.html"
