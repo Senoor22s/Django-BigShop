6fc0d187ad672af59d2b4ae65ad14c20155698ad
Senoor22s <legendsdt2@gmail.com>
2025-08-19 10:58:05 +0330

    32

--- core/order/models.py
+++ core/order/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class OrderStatusType(models.IntegerChoices):
+    pending = 1 , _("در انتظار پرداخت")
+    processing = 2, _("در حال پردازش")
+    shipped = 3, _("ارسال شده")
+    delivered = 4, _("تحویل شده")
+    canceled = 5, _("لغو شده")
+
+class UserAddressModel(models.Model):
+    user = models.ForeignKey('accounts.User',on_delete=models.CASCADE)
+    address = models.CharField(max_length=250)
+    state = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=50)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+class CouponModel(models.Model):
+    code = models.CharField(max_length=100)
+    discount_percent = models.IntegerField(default=0,validators = [MinValueValidator(0),MaxValueValidator(100)])
+    max_limit_usage = models.PositiveIntegerField(default=10)
+    used_by = models.ManyToManyField('accounts.User',related_name = "coupon_users")
+    expiration_date = models.DateTimeField(null=True,blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+class OrderModel(models.Model):
+    user = models.ForeignKey('accounts.User',on_delete=models.PROTECT)
+    address = models.CharField(max_length=250)
+    state = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=50)
+    total_price = models.DecimalField(default=0,max_digits=10,decimal_places=0)
+    coupon = models.ForeignKey(CouponModel,on_delete=models.PROTECT,null=True,blank=True)
+    status = models.IntegerField(choices=OrderStatusType.choices,default=OrderStatusType.pending.value)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    
+class OrderItemModel(models.Model):
+    order = models.ForeignKey(OrderModel,on_delete=models.CASCADE) 
+    product = models.ForeignKey('shop.ProductModel',on_delete=models.PROTECT)
+    quantity = models.PositiveIntegerField(default=0)
+    price = models.DecimalField(default=0,max_digits=10,decimal_places=0)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.product.title} - {self.cart.id}"
+    
